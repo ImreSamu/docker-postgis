e69288001f7db552464aff50dd4fedf47952dfe6
ImreSamu <ImreSamu@users.noreply.github.com>
2025-07-02 12:52:57 +0200

    fix manifest v30

--- test/postgis-config.sh
+++ test/postgis-config.sh
@@ -8,6 +8,10 @@ export IMAGE_NAME="${IMAGE_NAME:-postgis}"
 echo "Running tests for ${REGISTRY}/${REPO_NAME}/${IMAGE_NAME}"
 
 testAlias["${REGISTRY}/${REPO_NAME}/${IMAGE_NAME}"]=postgres
+
+# Architecture-specific images need postgres-initdb test too
+# The test builds a temporary image from our base image, which should work
+
 if [[ ${1} == *bundle* ]]; then
     imageTests["${REGISTRY}/${REPO_NAME}/${IMAGE_NAME}"]='
 		postgis-basics
